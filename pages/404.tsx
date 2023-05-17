02fb8e8435f0c26ab247b25e4f0df8534ca50de7
Elem <elem15@yandex.ru>
2023-05-17 18:24:15 +0300

    feat: add counter to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,24 @@
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
 export default function NotFountPage() {
+  const router = useRouter()
+  const [counter, setCounter] = useState(5)
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCounter(counter - 1)
+      if (counter === 1) {
+        clearInterval(interval)
+        router.push('/')
+      }
+    }, 1000)
+  }, [counter, router])
   return (
     <div className='not-found'>
       <h1>Ой... </h1>
       <h2>Такой страницы здесь нет</h2>
-      <p>Перейти на <Link href='/'>Главную станицу</Link></p>
+      <p>Перейхожу на <Link href='/'>Главную станицу</Link> через {counter} с.</p>
     </div>
   )
 }
