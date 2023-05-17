18da677413a0aea83cd94096d8e9fd1478f2305c
Elem <elem15@yandex.ru>
2023-05-18 00:49:14 +0300

    feat: add getStaticProps for static render

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-
-export default function Burgers() {
+import styles from '../../styles/Burger.module.css'
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:5000/items')
+  const data = await res.json()
+  return {
+    props: { burgers: data }
+  }
+}
+export default function Burgers({ burgers }: { burgers: Record<string, string>[] }) {
   return (
-    <div>Burgers</div>
+    <div>
+      <h1>
+        Наши бургеры
+      </h1>
+      {burgers.map(burger => (
+        <Link href={`/burger/${burger.id}`} key={burger.id} >
+          <span className={styles.burgerCard}>
+            <div className={styles.imageContainer}>
+              <Image src={burger.image} width='150' height="140" style={{ objectFit: 'cover' }} alt={burger.name} />
+            </div>
+            <div>
+              <h3>{burger.name}</h3>
+              <p>{burger.desc}</p>
+            </div>
+          </span>
+        </Link>
+      ))}
+    </div>
+
   )
 }
