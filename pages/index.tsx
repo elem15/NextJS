46b764ac5c5ed898f5c6ec494a5ee64fbc961b3e
Elem <elem15@yandex.ru>
2023-05-17 18:52:42 +0300

    feat: add Image & Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import classNames from 'classnames'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={classNames(styles.title, 'font-effect-fire-animation')}> Home</h1>
-      <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
-      <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
-      <Link href='/burgers'><a className={styles.btn}>Бургеры</a> </Link>
-    </div>
+    <>
+      <Head>
+        <title>Жирные бургеры | Главная страница</title>
+        <meta name='title' content='Жирные бургеры' />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={classNames(styles.title, 'font-effect-fire-animation')}> Home</h1>
+        <div className={styles.mainImage}>
+          <Image src="/fatburger.png" width={400} height={300} alt="fat burger" />
+        </div>
+        <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
+        <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
+        <Link href='/burgers'><span className={styles.btn}>Бургеры</span> </Link>
+      </div>
+    </>
   )
 }
