dd1f983005818fcafe38ebf9062cbc1e94a8092d
Elem <elem15@yandex.ru>
2023-05-18 20:50:02 +0300

    feat: add getStaticPaths to get params

--- pages/burgers/cheeseburger.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function cheeseburger() {
-  return (
-    <div>cheeseburger</div>
-  )
-}
--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import styles from '../../styles/Burger.module.css'
+
 export const getStaticProps = async () => {
   const res = await fetch('http://localhost:5000/items')
   const data = await res.json()
@@ -16,7 +17,7 @@ export default function Burgers({ burgers }: { burgers: Record<string, string>[]
         Наши бургеры
       </h1>
       {burgers.map(burger => (
-        <Link href={`/burger/${burger.id}`} key={burger.id} >
+        <Link href={`/burgers/${burger.id}`} key={burger.id} >
           <span className={styles.burgerCard}>
             <div className={styles.imageContainer}>
               <Image src={burger.image} width='150' height="140" style={{ objectFit: 'cover' }} alt={burger.name} />
